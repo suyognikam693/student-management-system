cd1f9dd2e7a8ab1102f119286e63eb26c094fc5b
Suyog Nikam <suyogwork1@gmail.com>
2026-03-04 10:19:28 +0530

    Refactored to arrow function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function login(user) {
     if (user.role === "student") {
         return "Student Dashboard";
@@ -7,5 +8,15 @@ function login(user) {
     }
     return "Unauthorized";
 }
+=======
+const login = (user) => {
+    switch (user.role) {
+        case "student":
+            return "Welcome Student";
+        default:
+            return "Access Denied";
+    }
+};
+>>>>>>> a97b800 (Refactored to arrow function)
 
 module.exports = login;
